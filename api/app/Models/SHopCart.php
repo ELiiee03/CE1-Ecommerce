c498df24d8b7158b2d5f717b90c4553df971c460
Lieca Eleccion <eleccionlieca03@gmail.com>
2024-09-24 18:05:45 +0800

    has established db connection

---
+++ api/app/Models/SHopCart.php
@@ -0,0 +1,11 @@
+<?php
+    class ShopCart {
+        private $conn;
+        private $table_name = "shopping_cart";
+
+        public $id;
+        public $cart_id;
+        
+    }
+
+?>
---
+++ initialize.php
@@ -0,0 +1,7 @@
+<?php
+
+    defined('DS') ? null : define
+
+
+
+?>
